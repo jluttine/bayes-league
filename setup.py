9d64b22ea832e39fa57a2a6ccecf13e55f1cdec6
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2023-05-09 16:19:33 +0300

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+if __name__ == "__main__":
+    setup(
+        install_requires=[
+            "numpy",
+            "scipy",
+            "jax",
+        ],
+        packages=find_packages(),
+        scripts=["manage.py"],
+        name="bayesleague",
+    )

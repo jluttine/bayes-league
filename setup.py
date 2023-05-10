665454c8ad6d63dce88cb6e1064c8f542e5d496b
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2023-05-10 10:37:07 +0300

    Include templates in the package

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup, find_packages
+from setuptools import setup, find_namespace_packages
 
 
 if __name__ == "__main__":
@@ -8,7 +8,11 @@ if __name__ == "__main__":
             "scipy",
             "autograd",
         ],
-        packages=find_packages(),
+        # NOTE: One needs to use find_namespace_packages instead of
+        # find_packages in order to include non-Python files (e.g., templates)
+        # in the package
+        packages=find_namespace_packages(),
+        include_package_data=True,
         scripts=["manage.py"],
         name="bayesleague",
     )

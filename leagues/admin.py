24d35cb8307a81014ebcad3aa2125f8301370b45
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2023-07-14 20:09:52 +0300

    Improve admin site

--- leagues/admin.py
+++ leagues/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 
-from .models import League, Player, Match, Ranking, RankingScore
+from .models import League, Player, Match, Ranking, RankingScore, Stage, Period
+
+
+class PeriodAdminInline(admin.TabularInline):
+    model = Period
+
+
+class MatchAdmin(admin.ModelAdmin):
+    inlines = [
+        PeriodAdminInline,
+    ]
 
 
 admin.site.register(League)
+admin.site.register(Stage)
 admin.site.register(Player)
-admin.site.register(Match)
+admin.site.register(Match, MatchAdmin)
 admin.site.register(Ranking)
 admin.site.register(RankingScore)

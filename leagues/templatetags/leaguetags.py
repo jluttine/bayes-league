26bff437bc92efebb9e1903c2f32fd7c86f68d86
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2025-07-29 23:37:35 +0300

    Don't show match date if it's today

--- leagues/templatetags/leaguetags.py
+++ leagues/templatetags/leaguetags.py
@@ -1,3 +1,4 @@
+import datetime
 from django.template import Library
 
 register = Library()
@@ -7,4 +8,28 @@ def repeat(string, times):
     return (
         "" if times is None or times == 0 or times == "" else
         string * times
+    )
+
+
+@register.filter
+def matchdate(m, show_date=True):
+    d = (
+        m.datetime_last_period if m.period_count > 0 else
+        m.datetime_started if m.datetime_started is not None else
+        m.datetime
+    )
+
+    if d is None:
+        return ""
+
+    # Convert to local time
+    dl = d.astimezone()
+    n = datetime.datetime.now().astimezone()
+
+    # Convert to string with special case for today
+    date = dl.strftime("%Y-%m-%d")
+    date_now = n.strftime("%Y-%m-%d")
+    return (
+        dl.strftime("%H:%M") if date == date_now else
+        dl.strftime(f"{date} %H:%M")
     )

9ea3a4b50124d2f79fc599b4dca36f976267ffb4
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2026-05-01 17:23:56 +0300

    Add django-ordered-model to shell.nix

--- shell.nix
+++ shell.nix
@@ -12,7 +12,32 @@ mkShell {
           };
         }
       ).withPackages (
-        ps: with ps; [
+        ps: with ps; let
+          django-ordered-model = buildPythonPackage rec {
+            pname = "django-ordered-model";
+            version = "3.7.4";
+            format = "pyproject";
+            src = fetchPypi {
+              inherit pname version;
+              hash = "sha256-8li5diUlwApTAJ6C+Li/KjqjFei0U+KB6P27/iuMs7o=";
+            };
+            nativeBuildInputs = [
+              setuptools
+            ];
+            checkInputs = [
+              djangorestframework
+            ];
+            propagatedBuildInputs = [
+              django
+            ];
+            checkPhase = ''
+              runHook preCheck
+              ${python.interpreter} -m django test --settings tests.settings
+              runHook postCheck
+            '';
+            pythonImportsCheck = [ "ordered_model" ];
+          };
+        in [
           build
           numpy
           scipy
